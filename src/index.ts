72947184688981abc07e62d13a93308c95e0ec14
Reshetov Denis <reshetovdenis@gmail.com>
2023-10-17 21:17:29 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { create as createHTTPClient } from 'ipfs-http-client';
+
+const ipfsHTTPClient = createHTTPClient({ url: 'http://ipfs.slonig.org:5001' });
+
+export async function getIPFSContentID(ipfs: any, content: string) {
+    const cid = await ipfs.dag.put(content, { storeCodec: 'dag-cbor', hashAlg: 'sha2-256' });
+    return cid.toString();
+}
+
+export async function getIPFSDataFromContentID(ipfs: any, cid: string) {
+    const chunks: Uint8Array[] = [];
+    for await (const chunk of ipfs.cat(cid)) {
+        chunks.push(Buffer.from(chunk));  // Ensure each chunk is a Buffer or Uint8Array
+    }
+    return Buffer.concat(chunks).toString();
+}
+
+async function addTextToIPFS(text: string): Promise<string | null> {
+    try {
+        const added = await ipfsHTTPClient.add(text);
+        console.log('Added text CID:', added.cid.toString());
+        const data = await getIPFSDataFromContentID(ipfsHTTPClient, added.cid.toString());
+        console.log('Data from IPFS:', data);
+        return added.cid.toString();
+    } catch (error) {
+        console.error('Error adding text to IPFS:', error);
+        return null;
+    }
+}
+
+(async () => {
+    const text: string = 'Denis-20231017-2024';
+    await addTextToIPFS(text);
+})();
